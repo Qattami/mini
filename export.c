3de4be074951549725fcf80a1b7a66dd2b6ab84b
iqattami <iqattami@e1r11p1.1337.ma>
2024-11-24 17:53:59 +0100

    hh

--- export.c
+++ export.c
@@ -39,17 +39,18 @@ int	strchr_idx(char *str, char character)
 
 void export_env0(char *key, char *value, f_env *lst) {
     f_env *tmp = lst;
+    f_env *tmp2 = lst;
     int key_exists = 0;
 
-    while (tmp) {
-        if (ft_strncmp(tmp->key, key, ft_strlen(key)) == 0) {  
-            free(tmp->value);
-            tmp->value = value ? ft_strdup(value) : NULL;  
-            key_exists = 1;
-            break;
-        }
-        tmp = tmp->next;
-    }
+    // while (tmp) {
+    //     if (ft_strncmp(tmp->key, key, ft_strlen(key)) == 0) {  
+    //         free(tmp->value);
+    //         tmp->value = value ? ft_strdup(value) : NULL;  
+    //         key_exists = 1;
+    //         break;
+    //     }
+    //     tmp = tmp->next;
+    // }
     if (!key_exists) 
         add_token(&lst, key, value);
 }
@@ -90,21 +91,22 @@ f_env *export_env(char *str, f_env *lst) {
     int append_idx = strchr_idx(str, '+');
     char *key = NULL;
     char *value = NULL;
+    char *new_value = NULL;
     
     if (append_idx != -1 && str[append_idx + 1] == '=') {
+
         key = strndup(str, append_idx);  // Get the key before '+='
         value = strdup(str + append_idx + 2);  // Get the value after '='
-        
-        // Find the key in the list and append the value if it exists
         f_env *tmp = lst;
         while (tmp) {
-            if (strcmp(tmp->key, key) == 0) {
-                char *new_value = ft_strjoin(tmp->value, value);
+            if (strcmp(tmp->key, key) == 0 && append_idx != -1) {
+                      
+                new_value = ft_strjoin(tmp->value, value);
                 free(tmp->value);
                 tmp->value = new_value;
                 free(key);
                 free(value);
-                return lst;
+                break;
             }
             tmp = tmp->next;
         }
@@ -122,11 +124,11 @@ f_env *export_env(char *str, f_env *lst) {
 
 
 int main(int ac, char **av, char **env) {
-    if (ac > 2) {  // Ensure av[2] exists
+    if (ac > 2) { 
         f_env *lst = NULL;
-        lst = loop_env(env);  // Assume loop_env initializes lst
+        lst = loop_env(env); 
         lst = export_env(av[2], lst);
-        print(lst);  // Print to verify
+        print(lst);  
     } else {
         printf("Usage: ./a.out <env_var>\n");
     }
